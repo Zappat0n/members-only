c3f06ec388d5dc5279cca6e513f3ef5757368e36
angel <albarros1973@gmail.com>
2021-01-08 13:32:32 +0100

    Create model and controller for Posts and Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
