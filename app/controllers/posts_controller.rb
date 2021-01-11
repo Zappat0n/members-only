c74ccfea2c8845173a02175b3def2de0d453c79e
angel <albarros1973@gmail.com>
2021-01-11 13:42:23 +0100

    Make changes requested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,11 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def new
-    @post = current_user.posts.build
+    @post = Post.new
   end
 
   def index
     @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def create
